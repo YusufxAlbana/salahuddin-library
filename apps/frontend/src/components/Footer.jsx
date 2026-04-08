3c32ded7c85bca7f1c9e624a289b6a48d5177fe1
Yusufelgebrak <yusufnawafalbana2009@gmail.com>
2026-04-08 09:56:33 +0700

    email

--- apps/frontend/src/components/Footer.jsx
+++ apps/frontend/src/components/Footer.jsx
@@ -48,12 +48,12 @@ function Footer() {
                             </a>
                         </li>
                         <li>
-                            <a href="mailto:shalahuddinlibrary@gmail.com" className="footer-contact-link">
+                            <a href="mailto:salahuddinlibrary@gmail.com" className="footer-contact-link">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                     <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                                     <polyline points="22,6 12,13 2,6"></polyline>
                                 </svg>
-                                <span>shalahuddinlibrary@gmail.com</span>
+                                <span>salahuddinlibrary@gmail.com</span>
                             </a>
                         </li>
                         <li>
