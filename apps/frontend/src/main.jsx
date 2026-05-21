ab9dec658443724bff664a979c52d923d9eb19f2
Yusufelgebrak <yusufnawafalbana2009@gmail.com>
2026-05-21 11:18:05 +0700

    chore: hapus ReactQueryDevtools (Tanstack) agar tidak muncul di production

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
 import { NotificationProvider } from './components/Notification'
@@ -50,7 +49,6 @@ createRoot(document.getElementById('root')).render(
           </BrowserRouter>
         </NotificationProvider>
       </AuthProvider>
-      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
     </QueryClientProvider>
   </StrictMode>,
 )
